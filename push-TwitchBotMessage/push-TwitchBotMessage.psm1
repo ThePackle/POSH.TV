2da24aedb9f63f60f498e71eeeeab426e11c0b58
ThePackle <packletwitch@gmail.com>
2020-04-12 14:50:11 -0400

    Delete push-TwitchBotMessage.psm1

--- push-TwitchBotMessage/push-TwitchBotMessage.psm1
+++
@@ -1,8 +0,0 @@
-function push-TwitchBotMessage{
-    param([Parameter(Mandatory=$true,Position=0)]$message)
-    
-    write-host $message
-    start-sleep -seconds 1
-    $global:writer.WriteLine("PRIVMSG $global:channel :$message")
-    $global:writer.Flush()
-}
